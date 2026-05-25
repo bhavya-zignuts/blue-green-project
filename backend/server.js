c3ad418b5f5f3459a01164bc20db5aef6b3570f3
ZTLAB57 <ztlab57@ZTLAB57s-Mac-mini.local>
2026-05-25 11:46:47 +0530

    health check added

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,38 @@
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
-const VERSION = process.env.VERSION || 'v1';
-const ENVIRONMENT = process.env.ENVIRONMENT || 'blue';
+// const VERSION = process.env.VERSION || 'v1';
+// const ENVIRONMENT = process.env.ENVIRONMENT || 'blue';
+
+// app.get('/', (req, res) => {
+//     res.send({
+//         message: 'Hello From Backend',
+//         version: VERSION,
+//         environment: ENVIRONMENT
+//     });
+// });
+
+// app.get('/health', (req, res) => {
+//     res.status(200).send('Backend Healthy');
+// });
+
+// app.listen(5000, () => {
+//     console.log('Backend Running on Port 5000');
+// });
+const express = require('express')
+
+const app = express()
+
+const PORT = 5000
 
 app.get('/', (req, res) => {
-    res.send({
-        message: 'Hello From Backend',
-        version: VERSION,
-        environment: ENVIRONMENT
-    });
-});
+    res.send('Hello From Backend V1')
+})
 
 app.get('/health', (req, res) => {
-    res.status(200).send('Backend Healthy');
-});
+    res.send('OK')
+})
 
-app.listen(5000, () => {
-    console.log('Backend Running on Port 5000');
-});
+app.listen(PORT, () => {
+    console.log(`Backend running on ${PORT}`)
+})
--- frontend/server.js
+++ frontend/server.js
@@ -1,21 +1,38 @@
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
-const VERSION = process.env.VERSION || 'v1';
-const ENVIRONMENT = process.env.ENVIRONMENT || 'blue';
+// const VERSION = process.env.VERSION || 'v1';
+// const ENVIRONMENT = process.env.ENVIRONMENT || 'blue';
+
+// app.get('/', (req, res) => {
+//     res.send(`
+//         <h1>Hello From Frontend</h1>
+//         <h2>Version: ${VERSION}</h2>
+//         <h2>Environment: ${ENVIRONMENT}</h2>
+//     `);
+// });
+
+// app.get('/health', (req, res) => {
+//     res.status(200).send('Frontend Healthy');
+// });
+
+// app.listen(3000, () => {
+//     console.log('Frontend Running on Port 3000');
+// });
+const express = require('express')
+
+const app = express()
+
+const PORT = 3000
 
 app.get('/', (req, res) => {
-    res.send(`
-        <h1>Hello From Frontend</h1>
-        <h2>Version: ${VERSION}</h2>
-        <h2>Environment: ${ENVIRONMENT}</h2>
-    `);
-});
+    res.send('Hello From Frontend V1')
+})
 
 app.get('/health', (req, res) => {
-    res.status(200).send('Frontend Healthy');
-});
+    res.send('OK')
+})
 
-app.listen(3000, () => {
-    console.log('Frontend Running on Port 3000');
-});
+app.listen(PORT, () => {
+    console.log(`Frontend running on ${PORT}`)
+})
